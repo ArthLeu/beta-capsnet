a848720d707872615181d2a3776b4aa2c3d11f92
ArthLeu <haoyanl2@uci.edu>
2020-11-18 14:05:43 +0800

    reordered files and upgrade train_ae.py

--- exp/helloworld.py
+++
@@ -1,4 +0,0 @@
-from collections import defaultdict
-
-if __name__ == "__main__":
-    print("hello world!")
--- main/AE/train_ae.py
+++ main/AE/train_ae.py
@@ -14,8 +14,11 @@ sys.path.append(os.path.abspath(os.path.join(BASE_DIR, '../../')))
 sys.path.append(os.path.abspath(os.path.join(BASE_DIR, '../../dataloaders')))
 
 import shapenet_part_loader
+import shapenet_core13_loader
+import shapenet_core55_loader
 from model import BetaPointCapsNet
 from solver import kl_divergence, reconstruction_loss
+from logger import Logger
 
 USE_CUDA = True
 
@@ -33,12 +36,25 @@ def main():
         capsule_net = torch.nn.DataParallel(capsule_net)
         capsule_net.to(device)
 
-    #create folder to save trained models
+    # create folder to save trained models
     if not os.path.exists(opt.outf):
         os.makedirs(opt.outf)
-        
-    train_dataset = shapenet_part_loader.PartDataset(classification=True, npoints=opt.num_points, split='train')
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=4)
+
+    # create folder to save logs
+    log_dir='./logs'+'/'+opt.dataset+'_dataset_'+str(opt.latent_caps_size)+'caps_'+str(opt.latent_vec_size)+'vec'+'_batch_size_'+str(opt.batch_size)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir);
+    logger = Logger(log_dir)
+
+    # select dataset    
+    if opt.dataset=='shapenet_part':
+        train_dataset = shapenet_part_loader.PartDataset(classification=True, npoints=opt.num_points, split='train')
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=4)        
+    elif opt.dataset=='shapenet_core13':
+        train_dataset = shapenet_core13_loader.ShapeNet(normal=False, npoints=opt.num_points, train=True)
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=4)
+    elif opt.dataset=='shapenet_core55':
+        train_dataset = shapenet_core55_loader.Shapnet55Dataset(batch_size=opt.batch_size, npoints=opt.num_points, shuffle=True, train=True)
 
     # BVAE CONFIGURATIONS HARDCODING
     #loss_mode = 'gaussian' # loss_mode was decoder_list in bVAE
@@ -55,64 +71,138 @@ def main():
     beta = 4            # default 4, pending addition to args
 
 
-    for epoch in range(opt.n_epochs):
-        if epoch < 50:
-            optimizer = optim.Adam(capsule_net.parameters(), lr=0.0001)
-        elif epoch<150:
-            optimizer = optim.Adam(capsule_net.parameters(), lr=0.00001)
-        else:
-            optimizer = optim.Adam(capsule_net.parameters(), lr=0.000001)
-        # train
-        capsule_net.train()
-        train_loss_sum = 0
+    # training process for 'shapenet_part' or 'shapenet_core13'
+    capsule_net.train()
+    if 'train_dataloader' in locals().keys() :
+        for epoch in range(opt.n_epochs):
+            if epoch < 50:
+                optimizer = optim.Adam(capsule_net.parameters(), lr=0.0001)
+            elif epoch<150:
+                optimizer = optim.Adam(capsule_net.parameters(), lr=0.00001)
+            else:
+                optimizer = optim.Adam(capsule_net.parameters(), lr=0.000001)
 
-        for batch_id, data in enumerate(train_dataloader):
-            points, _= data
-            if(points.size(0)<opt.batch_size):
-                break
-            points = Variable(points)
-            points = points.transpose(2, 1)
-            if USE_CUDA:
-                points = points.cuda()
+            #capsule_net.train()
+            train_loss_sum = 0
 
-            optimizer.zero_grad()
+            for batch_id, data in enumerate(train_dataloader):
+                global_iter += 1
+
+                points, _= data
+                if(points.size(0)<opt.batch_size):
+                    break
+                points = Variable(points)
+                points = points.transpose(2, 1)
+                if USE_CUDA:
+                    points = points.cuda()
+    
+                optimizer.zero_grad()
+                
+                # ---- CRITICAL PART: new train loss computation (train_loss in bVAE was beta_vae_loss)
+                x_recon, mu, logvar = capsule_net(points) # returns x_recon, mu, logvar
+                recon_loss = reconstruction_loss(points, x_recon, loss_mode) # RECONSTRUCTION LOSS
+                total_kld, dim_wise_kld, mean_kld = kl_divergence(mu, logvar) # DIVERGENCE
+
+                if loss_objective == 'H':
+                    train_loss = recon_loss + beta*total_kld
+                elif loss_objective == 'B':
+                    C = torch.clamp(C_max/C_stop_iter*global_iter, 0, C_max.data[0])
+                    train_loss = recon_loss + gamma*(total_kld-C).abs()
+                
+                # original train loss computation (deprecated)
+                #train_loss = capsule_net.module.loss(points, x_recon)
+                #train_loss.backward()
+
+                # combining per capsule loss (pyTorch requires)
+                scalar_loss = train_loss.sum()
+                scalar_loss.backward()
+                optimizer.step()
+                train_loss_sum += scalar_loss
+
+                # ---- END OF CRITICAL PART ----
+                
+                info = {'train_loss': scalar_loss.item()}
+               
+                for tag, value in info.items():
+                    logger.scalar_summary(
+                        tag, value, (len(train_dataloader) * epoch) + batch_id + 1)                
+              
+                if batch_id % 50 == 0:
+                    print('bactch_no:%d/%d, train_loss: %f ' %  (batch_id, len(train_dataloader), scalar_loss.item()))
+    
+            print('Average train loss of epoch %d : %f' %
+                  (epoch, (train_loss_sum / len(train_dataloader))))
+
+            if epoch% 5 == 0:
+                dict_name = "%s/%s_dataset_%dcaps_%dvec_%d.pth"%\
+                    (opt.outf, opt.dataset, opt.latent_caps_size, opt.latent_vec_size, epoch)
+                torch.save(capsule_net.module.state_dict(), dict_name)  
+
+
+    # training process for 'shapenet_core55'
+    else:
+        for epoch in range(opt.n_epochs):
+            if epoch < 20:
+                optimizer = optim.Adam(capsule_net.parameters(), lr=0.001)
+            elif epoch<50:
+                optimizer = optim.Adam(capsule_net.parameters(), lr=0.0001)
+            else:
+                optimizer = optim.Adam(capsule_net.parameters(), lr=0.00001)
+        
+            #capsule_net.train()
+            train_loss_sum = 0
+
+            while train_dataset.has_next_batch():
+                global_iter += 1
+
+                batch_id, points_= train_dataset.next_batch()
+                points = torch.from_numpy(points_)
+                if(points.size(0)<opt.batch_size):
+                    break
+                points = Variable(points)
+                points = points.transpose(2, 1)
+                if USE_CUDA:
+                    points = points.cuda()
+
+                optimizer.zero_grad()
+
+                # ---- CRITICAL PART: same as above
+                x_recon, mu, logvar = capsule_net(points)
+                recon_loss = reconstruction_loss(points, x_recon, loss_mode)
+                total_kld, dim_wise_kld, mean_kld = kl_divergence(mu, logvar)
+
+                if loss_objective == 'H':
+                    train_loss = recon_loss + beta*total_kld
+                elif loss_objective == 'B':
+                    C = torch.clamp(C_max/C_stop_iter*global_iter, 0, C_max.data[0])
+                    train_loss = recon_loss + gamma*(total_kld-C).abs()
+
+                scalar_loss = train_loss.sum()
+                scalar_loss.backward()
+                optimizer.step()
+                train_loss_sum += scalar_loss
+                # ---- END OF CRITICAL PART ----       
+    
+                info = {'train_loss': scalar_loss.item()}
+                for tag, value in info.items():
+                    logger.scalar_summary(
+                        tag, value, (int(57448/opt.batch_size) * epoch) + batch_id + 1)
+                info = {'train_loss': scalar_loss.item()}
+                for tag, value in info.items():
+                    logger.scalar_summary(
+                        tag, value, (int(57448/opt.batch_size) * epoch) + batch_id + 1)
+                    
+                if batch_id % 50 == 0:
+                    print('bactch_no: %d / %d at epoch %d; train_loss: %f ' %  (batch_id, int(57448/opt.batch_size),epoch,scalar_loss.item() )) # the dataset size is 57448
             
-            # ---- CRITICAL PART: new train loss computation (train_loss in bVAE was beta_vae_loss)
-            x_recon, mu, logvar = capsule_net(points) # returns x_recon, mu, logvar
-            recon_loss = reconstruction_loss(points, x_recon, loss_mode) # RECONSTRUCTION LOSS
-            total_kld, dim_wise_kld, mean_kld = kl_divergence(mu, logvar) # DIVERGENCE
+            print('Average train loss of epoch %d : %f' % (epoch, (train_loss_sum / int(57448/opt.batch_size))))    
 
-            if loss_objective == 'H':
-                train_loss = recon_loss + beta*total_kld
-            elif loss_objective == 'B':
-                C = torch.clamp(C_max/C_stop_iter*global_iter, 0, C_max.data[0])
-                train_loss = recon_loss + gamma*(total_kld-C).abs()
-            
-            # original train loss computation (deprecated)
-            #train_loss = capsule_net.module.loss(points, x_recon)
-            #train_loss.backward()
+            train_dataset.reset()
 
-            # combining per capsule loss (pyTorch requires)
-            scalar_loss = train_loss.sum()
-            scalar_loss.backward()
-            train_loss_sum += scalar_loss
-
-            # ---- END OF CRITICAL PART ----
-
-            optimizer.step()
-
-            if batch_id % 50 == 0:
-                print('batch_no: %d / %d, train_loss: %f ' %  (batch_id, len(train_dataloader), scalar_loss.item()))
-
-        print('Average train loss of epoch %d : %f' %
-              (epoch, (train_loss_sum / len(train_dataloader))))
-
-        if epoch % 5 == 0:
-            dict_name = "%s/%s_dataset_%dcaps_%dvec_%d.pth"%\
-                (opt.outf, opt.dataset, opt.latent_caps_size, opt.latent_vec_size, epoch)
-            torch.save(capsule_net.module.state_dict(), dict_name)
-
-    global_iter += 1
+            if epoch% 5 == 0:
+                dict_name = "%s/%s_dataset_%dcaps_%dvec_%d.pth"%\
+                    (opt.outf, opt.dataset, opt.latent_caps_size, opt.latent_vec_size, epoch)
+                torch.save(capsule_net.module.state_dict(), dict_name)
 
 
 
