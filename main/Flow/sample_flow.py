3de89e34e341ba1bdda66a748ec4b637555855f4
F Zhong <zfc946@gmail.com>
2021-04-08 17:59:09 +0100

    small changes

--- main/Flow/sample_flow.py
+++ main/Flow/sample_flow.py
@@ -2,13 +2,16 @@ import torch
 from utils import gen_inn
 
 BATCHSIZE = 1
-N_DIM = 64*64
+CAP_DIM = 64
+N_CAP = 64
+N_DIM = CAP_DIM*N_CAP
+model_path = "/home/fz261/gfxdisp_fz/flow_models/flow_airplane.pt"
 
 inn = gen_inn(N_DIM)
-inn.load_state_dict(torch.load('capsule_flow.pt'))
+inn.load_state_dict(torch.load(model_path))
 inn.eval()
 
 z = torch.randn(BATCHSIZE, N_DIM)
 samples, _ = inn(z, rev=True)
-samples = torch.reshape(samples, (1, 64, 64))
+samples = torch.reshape(samples, (1, CAP_DIM, N_CAP))
 print(samples)
--- main/Flow/train_flow.py
+++ main/Flow/train_flow.py
@@ -1,5 +1,6 @@
 import torch
 from utils import DataLoader, gen_inn
+import os
 
 BATCHSIZE = 20
 N_DIM = 64*64
@@ -7,15 +8,19 @@ N_DIM = 64*64
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dir_capsules = "/home/fz261/Repository/beta-capsnet/dataset/latent_capsules/airplane/"
 base_filename = "latcaps_airplane"
-save_path = "flow_airplane.pt"
+save_path = "/home/fz261/gfxdisp_fz/flow_models/flow_airplane.pt"
 
 inn = gen_inn(N_DIM)
 inn.to(device)
+if os.path.isfile(save_path):
+    inn.load_state_dict(torch.load(save_path))
+    inn.eval()
+    print('Resumed training from', save_path)
 
 dl = DataLoader(device, dir_capsules, base_filename, N_DIM)
 optimizer = torch.optim.Adam(inn.parameters(), lr=0.001)
 
-for i in range(10000):
+for i in range(100000):
     optimizer.zero_grad()
 
     x = dl.next_batch(BATCHSIZE)
@@ -30,5 +35,5 @@ for i in range(10000):
     optimizer.step()
 
     print(i, loss.item(), end='\r')
-    if i%500==0:
+    if i%1000==0:
         torch.save(inn.state_dict(), save_path)
